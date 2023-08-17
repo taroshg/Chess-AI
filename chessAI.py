2fd97962e7b87f33ecd014e7c156d4aa729ae01c
taroshg <tarosh.g@gmail.com>
2023-08-17 01:39:21 -0400

    major improvements

--- chessAI.py
+++ chessAI.py
@@ -1,385 +1,226 @@
-import chess, chess.polyglot, chess.engine
-# import random
+import chess, chess.polyglot, chess.engine, chess.pgn
+from chess import Move, PieceType, Square
+from typing import Optional, Callable, Iterator
+import random
 import math
+from IPython.display import display, clear_output
 
-# largest number
-MAX = math.inf
+class TranspositionTableEntry:
+    def __init__(self, depth, value, flag):
+        self.depth = depth
+        self.value = value
+        self.flag = flag
+        
+class TranspositionTable:
+    def __init__(self):
+        self.table = {}
+        self.n_pos = 0
 
-# piece values
-piece_values = {chess.PAWN:100, chess.KNIGHT:320, chess.BISHOP:330, 
-                chess.ROOK:500, chess.QUEEN:900, chess.KING:0}
-# [pawn, knight, bishop, rook, queen, king] 
-pieces_square_table = [
-    [0,  0,  0,  0,  0,  0,  0,  0,
-    5, 10, 10,-20,-20, 10, 10,  5,
-    5, -5,-10,  0,  0,-10, -5,  5,
-    0,  0,  0, 20, 20,  0,  0,  0,
-    5,  5, 10, 25, 25, 10,  5,  5,
-    10, 10, 20, 30, 30, 20, 10, 10,
-    50, 50, 50, 50, 50, 50, 50, 50,
-    0,  0,  0,  0,  0,  0,  0,  0],
+    def add(self, hashcode: int, tt_entry: TranspositionTableEntry):
+        self.table[str(hashcode)] = tt_entry
 
-
-    [-50,-40,-30,-30,-30,-30,-40,-50,
-    -40,-20,  0,  5,  5,  0,-20,-40,
-    -30,  5, 10, 15, 15, 10,  5,-30,
-    -30,  0, 15, 20, 20, 15,  0,-30,
-    -30,  5, 15, 20, 20, 15,  5,-30,
-    -30,  0, 10, 15, 15, 10,  0,-30,
-    -40,-20,  0,  0,  0,  0,-20,-40,
-    -50,-40,-30,-30,-30,-30,-40,-50],
-
-    [-20,-10,-10,-10,-10,-10,-10,-20,
-    -10,  5,  0,  0,  0,  0,  5,-10,
-    -10, 10, 10, 10, 10, 10, 10,-10,
-    -10,  0, 10, 10, 10, 10,  0,-10,
-    -10,  5,  5, 10, 10,  5,  5,-10,
-    -10,  0,  5, 10, 10,  5,  0,-10,
-    -10,  0,  0,  0,  0,  0,  0,-10,
-    -20,-10,-10,-10,-10,-10,-10,-20],
-
-    [0,  0,  0,  5,  5,  0,  0,  0,
-    -5,  0,  0,  0,  0,  0,  0, -5,
-    -5,  0,  0,  0,  0,  0,  0, -5,
-    -5,  0,  0,  0,  0,  0,  0, -5,
-    -5,  0,  0,  0,  0,  0,  0, -5,
-    -5,  0,  0,  0,  0,  0,  0, -5,
-    5, 10, 10, 10, 10, 10, 10,  5,
-    0,  0,  0,  0,  0,  0,  0,  0],
-
-    [-20,-10,-10, -5, -5,-10,-10,-20,
-    -10,  0,  0,  0,  0,  0,  0,-10,
-    -10,  5,  5,  5,  5,  5,  0,-10,
-    0,  0,  5,  5,  5,  5,  0, -5,
-    -5,  0,  5,  5,  5,  5,  0, -5,
-    -10,  0,  5,  5,  5,  5,  0,-10,
-    -10,  0,  0,  0,  0,  0,  0,-10,
-    -20,-10,-10, -5, -5,-10,-10,-20],
-
-    [20, 30, 10,  0,  0, 10, 30, 20,
-    20, 20,  0,  0,  0,  0, 20, 20,
-    -10,-20,-20,-20,-20,-20,-20,-10,
-    -20,-30,-30,-40,-40,-30,-30,-20,
-    -30,-40,-40,-50,-50,-40,-40,-30,
-    -30,-40,-40,-50,-50,-40,-40,-30,
-    -30,-40,-40,-50,-50,-40,-40,-30,
-    -30,-40,-40,-50,-50,-40,-40,-30]]
-# king endgame square table
-king_endgame =  [-50,-30,-30,-30,-30,-30,-30,-50,
-                -30,-20,-10,  0,  0,-10,-20,-30,
-                -30,-10, 20, 30, 30, 20,-10,-30,
-                -30,-10, 30, 40, 40, 30,-10,-30,
-                -30,-10, 30, 40, 40, 30,-10,-30,
-                -30,-10, 20, 30, 30, 20,-10,-30,
-                -30,-30,  0,  0,  0,  0,-30,-30,
-                -50,-40,-30,-20,-20,-30,-40,-50]
-
-def get_opening_moves(board):
-    """
-    Returns list of moves from given opening book
-    """
-    moves = list()
-    with chess.polyglot.open_reader("opening/performance.bin") as reader:
-        for entry in reader.find_all(board):
-            moves.append(entry.move)
-    return moves
-
-
-def find_best_move(board, depth):
-    """
-    Finds the best move given a position and depth of analysis
-    """
-    opening_moves = get_opening_moves(board)
-    if len(opening_moves) != 0:
-        return opening_moves[0]
+    def get(self, hashcode: int) -> TranspositionTableEntry:
+        try:
+            return self.table[str(hashcode)]
+        except:
+            return None
+        
+def ai_move(board: chess.Board, depth, tt: TranspositionTable = None) -> chess.Move:
+    best_v = -math.inf
     best_move = None
 
-    if board.turn:
-        best_v = -MAX
-        for move in filtered_moves(board):
-            pos = move_result(board, move)
-            v = minimax(pos, depth, -MAX, MAX)
-            if best_v < v:
-                best_v = v
-                best_move = move
-    else:
-        best_v = MAX
-        for move in filtered_moves(board):
-            pos = move_result(board, move)
-            v = minimax(pos, depth, -MAX, MAX)
-            if best_v > v:
-                best_v = v
-                best_move = move
+    moves = ordered_moves(board)
+
+    for move in moves:
+        board.push(move)
+        v = -negamax(board, depth, -math.inf, math.inf, tt)
+        board.pop()
+        
+        # if game ending move was found
+        if v == math.inf:
+            best_move = move
+            return best_move
+
+        if v > best_v:
+            best_v = v
+            best_move = move
+
+    if best_move == None:
+        return random.choice(moves)
+
     return best_move
 
-
-def minimax(board, depth, alpha, beta):
-    """
-    Returns the best possible move
-    """
-    if depth == 0 or board.is_game_over():
-        return evaluation(board)
-
-    if board.turn:
-        v = -MAX
-        for move in filtered_moves(board):
-            pos = move_result(board, move)
-            v = max(v, minimax(pos, depth - 1, alpha, beta))
-            alpha = max(alpha, v)
+def negamax(board: chess.Board, depth: int, alpha: float, beta: float, 
+            tt: TranspositionTable = None) -> float:
+    if tt is not None:
+        hashcode = hash(board._transposition_key())
+        alpha_org = alpha
+        tt_entry = tt.get(hashcode)
+        if tt_entry is not None and tt_entry.depth >= depth:
+            if tt_entry.flag == "EXACT":
+                return tt_entry.value
+            elif tt_entry.flag == "LOWERBOUND":
+                alpha = max(alpha, tt_entry.value)
+            elif tt_entry.flag == "UPPERBOUND":
+                beta = min(beta, tt_entry.value)
             if alpha >= beta:
-                break
-        return v
-    else:
-        v = MAX
-        for move in filtered_moves(board):
-            pos = move_result(board, move)
-            v = min(v, minimax(pos, depth - 1, alpha, beta))
-            beta = min(beta, v)
-            if beta <= alpha:
-                break
-        return v
+                return tt_entry.value
 
+    if depth == 0 or board.is_game_over():
+        if tt is not None:
+            tt.n_pos += 1
+        return negamax_captures(board, alpha, beta)
+        return calc_eval(board) if board.turn else -calc_eval(board)
+    
+    v = -math.inf
+    for move in ordered_moves(board):
+        board.push(move)
+        v = max(v, -negamax(board, depth-1, -beta, -alpha, tt))
+        board.pop()
 
-def evaluation(board):
-    """
-    Returns the value of a given static position
-    """
+        alpha = max(alpha, v)
+        if alpha >= beta:
+            break
+    
+    if tt is not None:
+        flag = "EXACT"
+        tt_entry = TranspositionTableEntry(depth, v, flag)
+        tt_entry.value = v
+        if v <= alpha_org:
+            flag = "UPPERBOUND"
+        elif v >= beta:
+            flag = "LOWERBOUND"
+        tt_entry.flag = flag
+        tt.add(hashcode, tt_entry)
+
+    return v
+    
+def negamax_captures(board: chess.Board, alpha: float, beta: float) -> float:
+    v = calc_eval(board) if board.turn else -calc_eval(board)
+    if v > beta:
+        return beta
+
+    alpha = max(alpha, v)
+    
+    for move in ordered_moves(board, only_captures=True):
+        board.push(move)
+        v = -negamax_captures(board, -beta, -alpha)
+        board.pop()
+
+        if v >= beta:
+            return beta
+        alpha = max(alpha, v)
+
+    return alpha
+
+def calc_eval(board: chess.Board):
     if board.is_game_over():
         if board.result() == "1-0":
-            return MAX
+            return math.inf
         elif board.result() == "0-1":
-            return -MAX
+            return -math.inf
         else:
             return 0
-
+            
     v = 0
+    for s in chess.scan_reversed(board.occupied & chess.BB_ALL):
+        row = s // 8
+        col = s % 8
 
-    piece_map = board.piece_map()
-
-    filtered_piece_map = filtered_pieces(board, piece_map)
-
-    white_piece_map = filtered_piece_map[0]
-    black_piece_map = filtered_piece_map[1]
-    tense_piece_map = filtered_piece_map[2]
-
-    tense_pieces = set(tense_piece_map.keys())
-
-    endgame = False
-    if endgame == False:
-        endgame = True if game_phase(board, piece_map) < 1 else False
-
-    for s in piece_map:
-
-        piece = piece_map[s]
-
-        # White eval (+)
-        if piece.color == chess.WHITE:
-
-            v += piece_value(piece)
-            v += piece_square_value(board, s, piece, endgame)
-
-            # check for hanging pieces if any pieces  
-            if s in tense_pieces:
-                v -= hanging(board, s, piece, black_piece_map)
-
-        # Black eval (-)
-        else:
-
-            v -= piece_value(piece)
-            v -= piece_square_value(board, s, piece, endgame)
-
-            # check for hanging pieces if any pieces 
-            if s in tense_pieces:
-                v += hanging(board, s, piece, black_piece_map)
-
-    """
-    WHITE
-    """
-    # -bouns if checked
-    v -= 50 if board.is_check() else 0
-
-    # bouns for mobility
-    if not endgame:
-        v += board.legal_moves.count()
-    else:
-        v += board.legal_moves.count() * 2
-    
-    # -bouns for number of doubled pawns
-    v -= pawns_doubled(board, chess.WHITE) * 16
-
-    """
-    BLACK
-    """   
-    # -bouns if checked
-    v += 50 if board.is_check() else 0
-
-    # bouns for mobility
-    if not endgame:
-        v -= board.legal_moves.count()
-    else:
-        v -= board.legal_moves.count() * 2
-
-    # -bouns for number of doubled pawns
-    v += pawns_doubled(board, chess.BLACK) * 16
+        piece = board.piece_at(s)
+        v += piece_value(piece)
+        v += psqt_bonus(row, col, piece)
+        v += 69 * hanging(board, s, piece)
 
     return v
 
+def hanging(board: chess.Board, sqr: chess.Square, piece: chess.Piece = None):
+    w_attackers = board.attackers(chess.WHITE, sqr)
+    b_attackers = board.attackers(chess.BLACK, sqr)
 
-def piece_value(piece):
-    """
-    Given a piece this function returns its value
-    """
-    return piece_values[piece.piece_type]
+    if piece is None:
+        piece = board.piece_at(sqr)
 
-
-def pawns_doubled(board, color):
-    """
-    Returns number of pawns that are doubled and open
-    """
-    last_pawn_rank = set(range(48, 56)) if color else set(range(8, 16))
-
-    pawns = board.pieces(chess.PAWN, color)
-    doubled = 0
-
-    for s in pawns:
-        cur_s = s
-        while(cur_s not in last_pawn_rank):
-            check_s = cur_s + 8 if color else cur_s - 8
-            if check_s in pawns:
-                doubled += 1
-            cur_s = check_s
-
-    return doubled
-
-
-def hanging(board, square, piece, piece_map):
-    """
-    Returns value if piece is attacked and has no defenders
-    """
-    defender_squares = defenders(board, square, piece, piece_map)
-    d_count = len(defender_squares)
-    attacker_squares = board.attackers(not piece.color, square)
-    a_count = len(attacker_squares)
-    if d_count < a_count:
-        a = 0
-        d = 0
-        for s in defender_squares:
-            piece = board.piece_at(s)
-            if piece.piece_type != chess.KING:
-                d += 1 / piece_value(piece)
-            else:
-                d += 1 / 9000
-        for s in attacker_squares:
-            piece = board.piece_at(s)
-            if piece.piece_type != chess.KING:
-                a += 1 / piece_value(piece)
-            else:
-                a += 1 / 9000
-    return 0
-
-
-def defenders(board, square, piece, piece_map):
-    """
-    Returns square set of defenders to a given square
-    """
-    defenders = set()
-    for s in piece_map:
-        if piece_map[s].color == piece.color:
-            piece_d_squares = board.attacks(s)
-            if square in piece_d_squares:
-                defenders.add(s)
-    
-    return defenders
-
-
-def piece_square_value(board, square, piece, endgame):
-    """
-    Returns a value given the piece and square its on
-    Square values range from (0 - 63) [a1 value, ..., h8 value]
-    """
     v = 0
-
-    piece_index = [chess.PAWN, chess.KNIGHT, chess.BISHOP, 
-                    chess.ROOK, chess.QUEEN, chess.KING]
-
-    i = piece_index.index(piece.piece_type)
-
-    if piece.color:
-        if piece.piece_type == chess.KING and endgame:
-            return king_endgame[square]
-        return pieces_square_table[i][square]
-    else: 
-        if piece.piece_type == chess.KING and endgame:
-            return king_endgame[chess.square_mirror(square)]
-        return pieces_square_table[i][chess.square_mirror(square)]
+    # check white hanging (better for black)
+    if len(w_attackers) == 0 and len(b_attackers) > 0 and piece.color:
+        v -= 1
+        
+    # check black hanging (better for white)
+    if len(b_attackers) == 0 and len(w_attackers) > 0 and not piece.color:
+        v += 1
 
     return v
 
-
-def game_phase(board, piece_map):
+def piece_value(piece: chess.Piece, mg: bool = True):
     """
-    Returns a number according to the game phase, 0 being endgame
+    returns value of a piece.
     """
-    middle_game_limit = 15258 
-    end_game_limit = 3915
+    piece_values = [124, 781, 825, 1276, 2538, 0] if mg else [206, 854, 915, 1380, 2682, 0]
+    return piece_values[piece.piece_type - 1] if piece.color else -piece_values[piece.piece_type - 1]
 
-    # non pawn material value
-    npm = 0
-    for s in piece_map:
-        if piece_map[s].symbol() not in ['p', 'P']:
-            npm += piece_value(piece_map[s])
+def psqt_bonus(row: int, col: int, piece: chess.Piece, mg: bool = True):
+        """
+        row: row # in bitboard (starts from top-left)
+        col: col # in bitboard (starts from top-left)
+        piece_idx: one char in 'PNBRQKpnbrqk'
+        mg: is it middle game?
+        """
+        piece_idx = piece.piece_type - 1 # -1 because it chess.PIECE_TYPES starts from 1
+        bonus = [
+        [[-175,-92,-74,-73],[-77,-41,-27,-15],[-61,-17,6,12],[-35,8,40,49],[-34,13,44,51],[-9,22,58,53],[-67,-27,4,37],[-201,-83,-56,-26]],
+        [[-53,-5,-8,-23],[-15,8,19,4],[-7,21,-5,17],[-5,11,25,39],[-12,29,22,31],[-16,6,1,11],[-17,-14,5,0],[-48,1,-14,-23]],
+        [[-31,-20,-14,-5],[-21,-13,-8,6],[-25,-11,-1,3],[-13,-5,-4,-6],[-27,-15,-4,3],[-22,-2,6,12],[-2,12,16,18],[-17,-19,-1,9]],
+        [[3,-5,-5,4],[-3,5,8,12],[-3,6,13,7],[4,5,9,8],[0,14,12,5],[-4,10,6,8],[-5,6,10,8],[-2,-2,1,-2]],
+        [[271,327,271,198],[278,303,234,179],[195,258,169,120],[164,190,138,98],[154,179,105,70],[123,145,81,31],[88,120,65,33],[59,89,45,-1]]
+        ] if mg else [
+        [[-96,-65,-49,-21],[-67,-54,-18,8],[-40,-27,-8,29],[-35,-2,13,28],[-45,-16,9,39],[-51,-44,-16,17],[-69,-50,-51,12],[-100,-88,-56,-17]],
+        [[-57,-30,-37,-12],[-37,-13,-17,1],[-16,-1,-2,10],[-20,-6,0,17],[-17,-1,-14,15],[-30,6,4,6],[-31,-20,-1,1],[-46,-42,-37,-24]],
+        [[-9,-13,-10,-9],[-12,-9,-1,-2],[6,-8,-2,-6],[-6,1,-9,7],[-5,8,7,-6],[6,1,-7,10],[4,5,20,-5],[18,0,19,13]],
+        [[-69,-57,-47,-26],[-55,-31,-22,-4],[-39,-18,-9,3],[-23,-3,13,24],[-29,-6,9,21],[-38,-18,-12,1],[-50,-27,-24,-8],[-75,-52,-43,-36]],
+        [[1,45,85,76],[53,100,133,135],[88,130,169,175],[103,156,172,172],[96,166,199,199],[92,172,184,191],[47,121,116,131],[11,59,73,78]]
+        ]
 
-    npm = max(end_game_limit, min(npm, middle_game_limit))
+        pbonus = [[0,0,0,0,0,0,0,0],[3,3,10,19,16,19,7,-5],[-9,-15,11,15,32,22,5,-22],[-4,-23,6,20,40,17,4,-8],
+        [13,0,-13,1,11,-2,-13,5],[5,-12,-7,22,-8,-5,-15,-8],[-7,7,-3,-13,5,-16,10,-8],[0,0,0,0,0,0,0,0]
+        ] if mg else[
+        [0,0,0,0,0,0,0,0],[-10,-6,10,0,14,7,-5,-19],[-10,-10,-10,4,4,3,-6,-4],[6,-2,-8,-4,-13,-12,-10,-9],[10,5,4,-5,-5,-5,14,9],
+        [28,20,21,28,30,7,6,13],[0,-11,12,21,25,19,4,7],[0,0,0,0,0,0,0,0]]
 
-    return ((npm - end_game_limit) * 128) / (middle_game_limit - end_game_limit)
-
-
-def filtered_pieces(board, piece_map):
-    """
-    Returns white, black and attacked pieces and their squares
-    """
-    tense = dict()
-    white = dict()
-    black = dict()
-    for s in piece_map:
-        piece = piece_map[s]
-        if piece.color:
-            white[s] = piece
+        if piece_idx == 0:
+            row = (7 - row) if piece.color else row # 7 - row, filps board row wise 
+            v = pbonus[row][col]
+        elif piece_idx > 0:
+            row = (7 - row) if piece.color else row
+            col = min(col, 7 - col) # this code mirrors left half of board
+            v = bonus[piece_idx-1][row][col]
         else:
-            black[s] = piece
-        if board.is_attacked_by(not piece.color, s):
-            tense[s] = piece
-    return [white, black, tense]
+            v = 0
 
+        v = v / 10 # reducing the factor because the maximum is near 199
 
-def filtered_moves(board):
-    moves = set()
-    for move in board.legal_moves:
-        pos = move_result(board, move)
+        return v if piece.color else -v
 
-        piece_map = pos.piece_map()
-        filtered_piece_map = filtered_pieces(pos, piece_map)
-        tense_pieces = filtered_piece_map[2]
+def ordered_moves(board: chess.Board, only_captures=False):
 
-        any_hanging = False
+    def move_score(m: chess.Move):
+        score = 0
+        move_piece = board.piece_at(m.from_square)
+        capture_piece = board.piece_at(m.to_square)
 
-        for s in piece_map:
-            piece = piece_map[s]
-            if piece.color == board.turn:
-                if len(defenders(pos, s, piece, piece_map)) == 0 and pos.is_attacked_by(not piece.color, s):
-                    any_hanging = True
-                    break
+        if capture_piece is not None:
+            # motivate lower pieces to capture higher piece
+            score += abs(piece_value(capture_piece)) - abs(piece_value(move_piece))
+        else:
+            # demotivates squares with more attackers and less defenders (no hanging!)
+            attackers = board.attackers(not move_piece.color, m.to_square)
+            defenders = board.attackers(move_piece.color, m.to_square)
+            if m.from_square in defenders:
+                defenders.remove(m.from_square)
 
-        if not any_hanging:
-            moves.add(move)
+            score += len(defenders) - len(attackers)
 
-    return moves
-
-
-def move_result(board, move):
-    """
-    Returns a board after a move is made
-    """
-    b = board.copy()
-    b.push(move)
-    return b
+        return score
+    if only_captures:
+        return sorted(board.generate_legal_captures(), key=move_score, reverse=True)
+    return sorted(board.generate_legal_moves(), key=move_score, reverse=True)
--- runner.py
+++ runner.py
@@ -6,26 +6,27 @@ import chessAI as ai
 import time
 # from pstats import SortKey
 
-DEPTH = 3
+DEPTH = 2
 
 def main():
     # initiate classical chess board
     board = chess.Board()
-    pvc(board)
+    cvc(board)
 
-def cvc(board):
+def cvc(board: chess.Board):
     game = chess.pgn.Game()
     node = game
     game_start = time.time()
+    tt = ai.TranspositionTable()
     while not board.is_game_over():
         move_start = time.time()
-        move = ai.find_best_move(board, DEPTH)
+        move = ai.ai_move(board, DEPTH, tt)
         board.push(move)
         print(board.fen())
         node = node.add_variation(move)
         print(game)
         print(f'seconds taken: {time.time() - move_start}')
-        print(f'eval: {ai.evaluation(board)}\n')
+        print(f'eval: {ai.calc_eval(board)}\n')
 
     print(f'total time: {time.time() - game_start}')
 
@@ -33,9 +34,10 @@ def cvc(board):
     print(board.result())
 
 
-def pvc(board):
+def pvc(board: chess.Board):
     game = chess.pgn.Game()
     node = game
+    tt = ai.TranspositionTable()
     player_color = input("pick (white or black): ").lower()
     if player_color == "white":
             player_color = chess.WHITE
@@ -45,14 +47,14 @@ def pvc(board):
         print("Error: invalid choice")
         quit()
     while not board.is_game_over():
-        print(f'eval: {ai.evaluation(board)}\n')
+        print(f'eval: {ai.calc_eval(board)}\n')
         if board.turn == player_color:
             move = chess.Move.from_uci(input("your move: "))
             if move in board.legal_moves:
                 board.push(move)
                 node = node.add_variation(move)
         else:
-            move = ai.find_best_move(board, DEPTH)
+            move = ai.ai_move(board, DEPTH, tt)
             board.push(move)
             node = node.add_variation(move)
             print("AI's move: " + chess.Move.uci(move))
